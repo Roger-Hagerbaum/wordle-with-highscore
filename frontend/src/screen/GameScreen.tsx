4dcfe5665fde603f0284c6caf3c9dcc8b4ae4a29
Roger-Hagerbaum <roger.hagerbaum@lerniastudent.se>
2023-06-04 22:12:24 +0200

    fixed timmer

--- frontend/src/screen/GameScreen.tsx
+++ frontend/src/screen/GameScreen.tsx
@@ -3,6 +3,7 @@ import {Game} from "../types";
 import GuessInput from "../component/GuessInput";
 import GuessBoxes from "../component/GuessBoxes";
 import GuessList from "../component/GuessList";
+import Timer from "../component/Timer";
 
 type GameScreenProps = {
     game:Game;
@@ -11,6 +12,7 @@ type GameScreenProps = {
 const GameScreen: React.FC<GameScreenProps> = ({game, onGuess}) =>{
     return (
         <div className={"GameScreen"}>
+            <Timer />
             <GuessInput onGuess={onGuess} wordLength={game.wordLength} />
             <GuessList guesses={game.guesses} />
 
